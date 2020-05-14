803de9b840e19dc706cbefa8df6608b0ff08cf5b
Jonathan Duque <johndduque@gmail.com>
2020-05-14 14:37:13 -0400

    add dashboard design

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,6 +2,18 @@ import React, { useContext, useState } from "react";
 import UserProvider from "./contexts/UserProvider";
 import _ from "lodash";
 
+import "antd/dist/antd.css";
+
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
 const LoginMsg =
   "Uh oh, there's nothing to show! " +
   "Login to see how much of your invaluable personal " +
@@ -10,16 +22,66 @@ const LoginMsg =
 const Profile = () => {
   const [selected, setSelected] = useState("All");
   const userData = useContext(UserProvider.context);
-  const text = _.isEmpty(userData) ? LoginMsg : "Explore Your Data";
+  const text = userData.username;
 
   return (
-    <div className="page">
-      <p className="page-title" style={{ textAlign: "center" }}>
-        {text}
-      </p>
-
-      <div style={{ marginBottom: 20 }} />
-    </div>
+    <Layout>
+      <Header style={{ color: "#FFF", fontSize: "16px" }}>
+        MatchMocker
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ float: "right" }}
+        >
+          <Menu.Item key="1">dashboard</Menu.Item>
+          <Menu.Item key="2">interview listings</Menu.Item>
+          <Menu.Item key="3">make post</Menu.Item>
+          <Menu.Item key="4">contact us</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%", borderRight: 0 }}
+          >
+            <SubMenu
+              key="sub1"
+              icon={<UserOutlined />}
+              title={userData.username}
+            >
+              <Menu.Item key="1">Logout</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 };
 
