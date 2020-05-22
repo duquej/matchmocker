e5b56dcabd14059cc5dafe7f4fe0bc4e5ca1ff7c
Jonathan Duque <johndduque@gmail.com>
2020-05-22 13:13:30 -0400

    add analytics

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@
 import React, { Component } from "react";
 
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-import Home from "./Home";
 import Dashboard from "./Dashboard";
 import UserProvider from "./contexts/UserProvider";
 import { ProtectedRoute } from "./ProtectedRouter";
 import Withprotected from "./Withprotected";
 import Intermediary from "./Intermediary";
+import ReactGA from "react-ga";
+
+ReactGA.initialize(process.env.ANALYTICS_API_KEY);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
   constructor() {
--- configs/keysConfig.js
+++ configs/keysConfig.js
@@ -3,4 +3,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
   sendGridAPI: process.env.SENDGRID_API_KEY,
+  analyticsKey: process.env.ANALYTICS_API_KEY,
 };
